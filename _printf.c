a4977dc67a58bcff194988e1c8445705bcdf79c6
sclpist <68994787+sclpist@users.noreply.github.com>
2023-07-25 13:34:09 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -2,41 +2,55 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char *const format, ...)
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-			{"%s", printf_string}, {"%c", printf_char}, {"%%", printf_37}, {"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev}, {"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned}, {"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX}, {"%S", printf_exclusive_string}, {"%p", printf_pointer}};
-
 	va_list args;
-	int i = 0, j, len = 0;
-
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-Here:
-	while (format[i] != '\0')
+	int count = 0;
+
+	while (*format)
 	{
-		j = 13;
-		while (j >= 0)
+		if (*format == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			format++; // Move past the '%'
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				putchar('%');
+				count++;
 			}
-			j--;
+			else if (*format == 'c')
+			{
+				char c = (char)va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				const char *s = va_arg(args, const char *);
+				while (*s)
+				{
+					putchar(*s);
+					s++;
+					count++;
+				}
+			}
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+
+		format++;
 	}
+
 	va_end(args);
-	return (len);
+	return count;
 }
